1e284f8b782e0adde7b9567d4a617477f076553b
k1lLr3AlL <31995128+k1lLr3AlL@users.noreply.github.com>
2018-04-08 10:36:06 +0300

    add class Match

--- minigolf.py
+++ minigolf.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from abc import ABCMeta, abstractmethod
 
 
 class Player:
@@ -6,15 +7,43 @@ class Player:
         self.name = name
 
 
-class HitsMatch:
+class Match(metaclass=ABCMeta):
     def __init__(self, number_of_holes, arr_with_players):
         self.number_of_holes = number_of_holes
         self.arr_with_players = arr_with_players
         self.table = [tuple([player.name for player in arr_with_players])]
-        self.drop = [False for player in self.arr_with_players]
         self.lap = [None for player in self.arr_with_players]
         self.count = 0
         self.finished = False
+
+    def get_table(self):
+        result_table = self.table.copy()
+        if self.lap.count(None) != len(self.lap):
+            result_table.append(tuple(self.lap))
+        for hole in range(self.number_of_holes - len(result_table) + 1):
+            result_table.append(tuple([None for player in self.arr_with_players]))
+        return result_table
+
+    def get_winners(self):
+        table = self.get_table()[1:]
+        try:
+            table = [reduce(lambda x, y: x + y, i) for i in (zip(*table))]
+            if type(self) == HitsMatch:
+                return [self.arr_with_players[index] for index, score in enumerate(table) if score == min(table)]
+            elif type(self) == HolesMatch:
+                return [self.arr_with_players[index] for index, score in enumerate(table) if score == max(table)]
+        except TypeError:
+            raise RuntimeError
+
+    @abstractmethod
+    def hit(self, success=False):
+        pass
+
+
+class HitsMatch(Match):
+    def __init__(self, number_of_holes, arr_with_players):
+        Match.__init__(self, number_of_holes, arr_with_players)
+        self.drop = [False for player in self.arr_with_players]
         self.buff = [None for player in self.arr_with_players]
 
     def hit(self, success=False):
@@ -39,7 +68,7 @@ class HitsMatch:
                 self.lap[self.count] = self.buff[self.count]
 
             self.count += 1
-
+            # Условие перехода к следующей лунке
             if all(self.drop) or self.buff.count(9) > 0:
                 self.buff = [item + 1 if item == 9 else item for item in self.buff]
                 self.lap = self.buff
@@ -48,6 +77,7 @@ class HitsMatch:
                 # отчистить lap
                 self.lap = [None for player in self.arr_with_players]
                 self.buff = [None for player in self.arr_with_players]
+                # Определяю какой игрок делает следующий удар
                 if success:
                     if self.count == len(self.lap):
                         self.count = 0
@@ -58,36 +88,11 @@ class HitsMatch:
             if len(table) - 1 == self.number_of_holes and table[-1].count(None) == 0:
                 self.finished = True
 
-    def get_table(self):
-        result_table = self.table.copy()
-        if self.lap.count(None) != len(self.lap):
-            result_table.append(tuple(self.lap))
-        for hole in range(self.number_of_holes - len(result_table) + 1):
-            result_table.append(tuple([None for player in self.arr_with_players]))
 
-        return result_table
-
-    def get_winners(self):
-        table = self.get_table()[1:]
-        try:
-            table = [reduce(lambda x, y: x + y, i) for i in (zip(*table))]
-            return [self.arr_with_players[index] for index, score in enumerate(table) if score == min(table)]
-        except TypeError:
-            raise RuntimeError
-
-
-class HolesMatch:
+class HolesMatch(Match):
     def __init__(self, number_of_holes, arr_with_players):
-        self.number_of_holes = number_of_holes
-        self.arr_with_players = arr_with_players
-        self.table = [tuple([player.name for player in arr_with_players])]
-        # self.drop = [False for player in self.arr_with_players]
-        self.lap = [None for player in self.arr_with_players]
-        self.count = 0
-        self.finished = False
-        self.buff = [0 for player in self.arr_with_players]
+        Match.__init__(self, number_of_holes, arr_with_players)
         self.num_of_try = [0 for player in self.arr_with_players]
-
         self.num_of_hit = 0
         self.round = 0
 
@@ -100,44 +105,27 @@ class HolesMatch:
                 self.count = 0
             if success:
                 self.lap[self.count] = 1
-
             try:
                 self.num_of_try[self.count] += 1
             except TypeError:
                 self.num_of_try[self.count] = 1
-
             self.count += 1
+            # Условие перехода к следующей лунке
             if self.num_of_hit % len(self.arr_with_players) == 0 and (
-                    self.lap.count(1) > 0 or self.num_of_try.count(10) > 0):
+                            self.lap.count(1) > 0 or self.num_of_try.count(10) > 0):
+                # Определяю какой игрок делает следующий удар
                 self.round += 1
                 if self.round > len(self.arr_with_players):
                     self.count = 0
                 else:
                     self.count = self.round
-
+                # Сбрасываю переменные на дефолтные значения
                 self.table.append(tuple([0 if score is None else score for score in self.lap]))
                 self.lap = [None for player in self.arr_with_players]
                 self.num_of_try = [0 for player in self.arr_with_players]
             table = self.get_table()
             if len(table) - 1 == self.number_of_holes and table[-1].count(None) == 0:
                 self.finished = True
-
-    def get_table(self):
-        result_table = self.table.copy()
-        if self.lap.count(None) != len(self.lap):
-            result_table.append(tuple(self.lap))
-        for hole in range(self.number_of_holes - len(result_table) + 1):
-            result_table.append(tuple([None for player in self.arr_with_players]))
-
-        return result_table
-
-    def get_winners(self):
-        table = self.get_table()[1:]
-        try:
-            table = [reduce(lambda x, y: x + y, i) for i in (zip(*table))]
-            return [self.arr_with_players[index] for index, score in enumerate(table) if score == max(table)]
-        except TypeError:
-            raise RuntimeError
 
 
 
